bb8b7ccaa7463eb21b78c73093b58715b122caf5
SUJINJEONG012 <peekaboo32@naver.com>
2024-05-25 23:17:11 +0900

    order by로 정렬

--- 데이터조회하기/level1.sql
+++ 데이터조회하기/level1.sql
@@ -4,3 +4,8 @@ where manager_id is null;
 -- null이 아닌 값 조회
 select * from employees
 where manager_id is not null;
+
+-- 부서번호로 정렬 후, 사원번호로 정렬 
+select department_id, employee_id, first_name, last_name
+from employees
+order by department_id, employee_id;
