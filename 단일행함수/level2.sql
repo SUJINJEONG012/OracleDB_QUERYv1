ace1706bff9d5e5e924fc807508f01eb69bd71b1
SUJINJEONG012 <peekaboo32@naver.com>
2024-05-26 20:52:09 +0900

    데이터 함수 날짜, 문자형을 숫자로, 숫자타입을 문자타입으로 반환하는 쿼리

--- 단일행함수/level2.sql
+++ 단일행함수/level2.sql
@@ -41,3 +41,32 @@ select to_char(12345678, 'L999,999,999') local from dual;
 select to_char(123, '0999') from dual;
 -- 16진수로 변경
 select to_char(123, 'XXXX') from dual;
+
+
+
+
+-- Data타입의 데이터를 특정형식의 문자타입으로 변환
+select to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS') "sysdate"
+from dual;
+
+--356일 중 몇일 째인지 조회 
+select to_char(sysdate, 'DDD') "day of year" from dual;
+
+-- 53주 몇 주차 인지 조회
+select to_char(sysdate, 'IW') from dual;
+-- 해당 월의 이름 조회
+select to_char(sysdate, 'month') "몇달인지" from dual;
+select to_char(sysdate, 'DAY') "무슨요일인지" from dual;
+
+-- employees 테이블에서 department_id 가 10인 사원들의 입사일을 이용하여 월/년도로 출력 
+select department_id , to_char(hire_date, 'MM/YY') "입사일" from employees where department_id = 10;
+
+-- employees 테이블에서 salary가 10000 달러가 넘는 사원들의 이름과 월급을 월급이 많은 순으로 나타내어라.
+select  last_name 이름, to_char(salary, '$99,999,999.00') 월급 from employees
+order by salary desc;
+
+-- date 타입으로 변환하는 예제, 문자를 데이터 타입으로 변환
+select to_date('2011-01-01', 'YYYY-MM-DD') from dual;
+
+-- 문자를 숫자로 변환하는 간단한 예제
+select to_number('1021220') from dual;
