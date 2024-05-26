2bec1c615274e0719c239e15df88b612b23d7ad4
SUJINJEONG012 <peekaboo32@naver.com>
2024-05-26 18:12:50 +0900

    null값을 변경해주는 NVL함수

--- 단일행함수/level2.sql
+++ 단일행함수/level2.sql
@@ -8,4 +8,9 @@ case when salary < 9000 then '상위급여'
      when salary between 6000 and 8999 then '중위급여'
      else '하위급여'
 end "급여등급"
+from employees;
+
+-- nvl 함수는 null값을 다른값으로 바꿀때 사용, 모든 데이터 타입에 적용가능
+select last_name, nvl(manager_id,0) manager_id
 from employees
+where last_name ='King';
